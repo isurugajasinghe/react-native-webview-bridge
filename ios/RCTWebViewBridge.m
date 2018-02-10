797531580a64825fb147eca86d7990188c60fdd7
isurugajasinghe <isuruudyoga@gmail.com>
2018-02-10 08:19:13 +0530

    update header imports
    https://github.com/alinz/react-native-webview-bridge/issues/236

--- ios/RCTWebViewBridge.m
+++ ios/RCTWebViewBridge.m
@@ -14,13 +14,13 @@
 
 #import <UIKit/UIKit.h>
 
-#import <React/RCTAutoInsetsProtocol.h>
-#import <React/RCTConvert.h>
-#import <React/RCTEventDispatcher.h>
-#import <React/RCTLog.h>
-#import <React/RCTUtils.h>
-#import <React/RCTView.h>
-#import "UIView+React.h"
+#import "React/RCTAutoInsetsProtocol.h"
+#import "React/RCTConvert.h"
+#import "React/RCTEventDispatcher.h"
+#import "React/RCTLog.h"
+#import "React/RCTUtils.h"
+#import "React/RCTView.h"
+#import "React/UIView+React.h"
 #import <objc/runtime.h>
 
 //This is a very elegent way of defining multiline string in objective-c.
